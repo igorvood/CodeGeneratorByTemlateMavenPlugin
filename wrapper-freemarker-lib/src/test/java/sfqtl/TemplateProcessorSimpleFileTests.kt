766e1fe918b94b84bf12cd402c71b23ff65fb60e
Vood <balitsky_igor@list.ru>
2020-01-23 22:03:16 +0300

    Tests

--- wrapper-freemarker-lib/src/test/java/sfqtl/TemplateProcessorSimpleFileTests.kt
+++ wrapper-freemarker-lib/src/test/java/sfqtl/TemplateProcessorSimpleFileTests.kt
@@ -1,32 +1,28 @@
-package sfqtl;
+package sfqtl
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
-import java.util.stream.Stream;
-
-class TemplateProcessorSimpleFileTests extends AbstractTests {
-
-    private static Stream<Arguments> testData() {
-        return
-                Stream.of(
-                        "test_template_name",
-                        "collection_from_query",
-                        "clob_from_query",
-                        "struct_from_query",
-                        "sql_types_query",
-                        "sql_types_call",
-                        "rs_transponse",
-                        "test_static"
-                )
-                        .map(Arguments::of);
-    }
-
+internal class TemplateProcessorSimpleFileTests : AbstractTests() {
     @ParameterizedTest
     @MethodSource("testData")
-    void checkFtlFileProcessResult(String fileName) {
-        checkFtlFileProcessResult(fileName + ".txt", "sfqtl/" + fileName + ".ftl");
+    fun checkFtlFileProcessResult(fileName: String) {
+        checkFtlFileProcessResult("$fileName.txt", "sfqtl/$fileName.ftl")
     }
 
+    private fun testData(): Stream<Arguments> {
+        return Stream.of(
+                "test_template_name",
+                "collection_from_query",
+                "clob_from_query",
+                "struct_from_query",
+                "sql_types_query",
+                "sql_types_call",
+                "rs_transponse",
+                "test_static"
+        )
+                .map { arguments: String? -> Arguments.of(arguments) }
+    }
 }
