b22c3940d0cee244d2e326448967e6d3d6ab66d8
Vood <balitsky_igor@list.ru>
2020-01-25 18:19:40 +0300

    tests

--- code-generator-maven-plugin/src/test/java/ru/vood/generator/generate/runner/UtilKtTest.kt
+++ code-generator-maven-plugin/src/test/java/ru/vood/generator/generate/runner/UtilKtTest.kt
@@ -3,6 +3,7 @@ package ru.vood.generator.generate.runner
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import ru.vood.generator.generate.DataBaseConnectDto
 
 internal class UtilKtTest {
 
@@ -14,7 +15,7 @@ internal class UtilKtTest {
     fun resolveEngineRunner() {
         TemplateEngine.values().asList()
                 .stream()
-                .map { Pair(it, resolveEngineRunner(it)) }
+                .map { Pair(it, resolveEngineRunner(it, DataBaseConnectDto("oracle.jdbc.driver.OracleDriver", "jdbc:oracle:thin:@//localhost:1521/jp", "jp", "1"))) }
                 .forEach { Assertions.assertNotNull(it.second, "Engine runner for ${it.first} do not resolve") }
     }
 }
