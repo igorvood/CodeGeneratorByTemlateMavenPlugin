5d1024b54b99104c8c2f70146c0f6f3eabb1bc84
Vood <balitsky_igor@list.ru>
2020-01-25 12:07:43 +0300

    test

--- wrapper-freemarker-lib/src/test/java/ru/vood/freemarker/ext/processor/AbstractFtlProcessorTest.kt
+++ wrapper-freemarker-lib/src/test/java/ru/vood/freemarker/ext/processor/AbstractFtlProcessorTest.kt
@@ -1,16 +1,17 @@
 package ru.vood.freemarker.ext.processor
 
 import freemarker.template.Template
-import io.mockk.MockKAnnotations
-import io.mockk.every
+import io.mockk.*
 import io.mockk.impl.annotations.MockK
-import io.mockk.spyk
-import io.mockk.verify
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.provider.Arguments
 import java.io.Writer
+import java.util.function.Consumer
+import java.util.stream.Stream
 
-open class AbstractFtlProcessorTest {
+internal open class AbstractFtlProcessorTest {
 
     private lateinit var abstractFtlProcessorTest: AbstractFtlProcessor
 
@@ -27,10 +28,83 @@ open class AbstractFtlProcessorTest {
     @BeforeEach
     open fun setUp() {
         abstractFtlProcessorTest = spyk(TestAbstraction())
+    }
 
+    @AfterEach
+    fun afterTests() {
+        unmockkAll()
+        // or unmockkObject(MockObj)
+    }
+
+    //    @JvmStatic
+    open fun testData(): Stream<Arguments> {
+        return Stream.of(
+                Arguments.of("test_template_name", Consumer<Any> { abstractFtlProcessorTest.process("") }),
+                Arguments.of("collection_from_query", Consumer<Any> { abstractFtlProcessorTest.process("") }),
+                Arguments.of("clob_from_query", Consumer<Any> { abstractFtlProcessorTest.process("") }),
+                Arguments.of("struct_from_query", Consumer<Any> { abstractFtlProcessorTest.process("") })
+        )
+    }
+
+    val data = Stream.of(
+            Pair("test_template_name", Consumer<Any> { abstractFtlProcessorTest.process("") }),
+            Pair("collection_from_query", Consumer<Any> { abstractFtlProcessorTest.process("") })
+    )
+
+    @Test
+    fun process_1() {
+        testRunner(Consumer<Any> { abstractFtlProcessorTest.process(template) })
     }
 
     @Test
+    fun process_2() {
+        testRunner(Consumer<Any> { abstractFtlProcessorTest.process("templateName") })
+    }
+
+    @Test
+    fun process_3() {
+        testRunner(Consumer<Any> { abstractFtlProcessorTest.process(this::class.java, "templateName") })
+    }
+
+    @Test
+    fun process_4() {
+        testRunner(Consumer<Any> { abstractFtlProcessorTest.process("templateName", "templateBody") })
+    }
+
+    @Test
+    fun process_5() {
+        testRunner(Consumer<Any> { abstractFtlProcessorTest.process("templateName", mapOf()) })
+    }
+
+    @Test
+    fun process_6() {
+        testRunner(Consumer<Any> { abstractFtlProcessorTest.process(this::class.java, "templateName", mapOf()) })
+    }
+
+    @Test
+    fun process_7() {
+        testRunner(Consumer<Any> { abstractFtlProcessorTest.process("templateName", "", mapOf()) })
+    }
+
+/*
+
+    fun process(templateName: String, templateBody: String, param: Map<String, Any>): String {
+        param.forEach { registerSharedVar(it.key, it.value) }
+        return process(getTemplateFromString(templateName, templateBody))
+    }
+*/
+
+
+    private fun testRunner(cons: Consumer<Any>) {
+        every { abstractFtlProcessorTest.getTemplate(any()) } returns template
+        every { abstractFtlProcessorTest.process(template, writer) } answers { nothing }
+        cons.accept("")
+        //        abstractFtlProcessorTest.process("")
+        verify(atLeast = 1, atMost = 1) { abstractFtlProcessorTest.process(allAny<Template>(), allAny<Writer>(), *anyVararg<Any>()) }
+        unmockkAll()
+    }
+
+/*    @Test
     fun getTemplate() {
     }
 
@@ -42,22 +116,6 @@ open class AbstractFtlProcessorTest {
     fun getTemplateFromString() {
     }
 
-    @Test
-    fun process() {
-        every { abstractFtlProcessorTest.getTemplate(any()) } returns template
-        every { abstractFtlProcessorTest.process(template, writer) } answers { nothing }
-
-        abstractFtlProcessorTest.process("")
-        verify(atLeast = 1, atMost = 1) { abstractFtlProcessorTest.process(allAny<Template>(), allAny<Writer>(), *anyVararg<Any>()) }
-
-
-//        confirmVerified(abstractFtlProcessorTest)
-
-//        verify(abstractFtlProcessorTest, times(1)).process(template)
-//        abstractFtlProcessorTest.process("1")
-//        verify(abstractFtlProcessorTest, times(1)).process(template)
-
-    }
 
     @Test
     fun testProcess() {
@@ -73,7 +131,7 @@ open class AbstractFtlProcessorTest {
 
     @Test
     fun getGetStaticMethod() {
-    }
+    }*/
 
     open class TestAbstraction : AbstractFtlProcessor()
 }
